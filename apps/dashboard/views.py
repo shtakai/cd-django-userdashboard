c66e9f60966bcce923e25b25881bac285e783c67
Alyson Takai <shtakai@gmail.com>
2016-06-01 16:03:44 -0700

    implemented login use username

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import forms
+from django.contrib.auth import forms, login, authenticate
 from django.views.generic import View
 from django.views.generic.base import RedirectView
 from django.contrib import messages
@@ -42,3 +42,39 @@ class MainView(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
         print('MainView#get_redirect_url')
         return super(MainView, self).get_redirect_url(*args, **kwargs)
+
+
+class LoginView(View):
+    form = forms.AuthenticationForm
+
+    def get(self, request):
+        print('LoginView#get')
+        context = {
+            'form': self.form(),
+        }
+        return render(request, 'dashboard/login.html', context)
+
+    def post(self, request):
+        print('LoginView#POST', request.POST)
+        form = self.form(None, request.POST)
+        context = {
+            'form': form,
+        }
+        if form.is_valid():
+            print('form validation ok')
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            print('user', user)
+            if user is not None:
+                login(request, user)
+                print('login ok')
+                messages.add_message(request, messages.SUCCESS, 'Logged in')
+                return redirect('/main')
+            else:
+                messages.add_message(request, messages.WARNING, 'Failed log in')
+                return render(request, 'dashboard/login.html', context)
+        else:
+            messages.add_message(request, messages.WARNING, 'Failed log in')
+            return render(request, 'dashboard/login.html', context)
+
